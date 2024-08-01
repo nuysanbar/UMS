b2bf2baaa9d1c79f8e1609b59c591b6d89b4e3b8
Nuysanbar <102357504+nuysanbar@users.noreply.github.com>
2024-08-01 21:53:56 +0300

    Update adminLA.jsx

--- ums-frontend/src/routes/Admin/adminLA.jsx
+++ ums-frontend/src/routes/Admin/adminLA.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 const access_token=window.localStorage.getItem('access_token');
 export async function usersLoader(){
+    // make fixed apiurl errors
     const apiUrl=`http://localhost:3500/admin/getCustomers`
     const res = await axios.get(apiUrl,{
         headers: {
